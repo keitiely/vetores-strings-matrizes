f93564832a3e9de47ebb24f90195de6a5d63274b
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-10-28 19:51:02 -0300

    Update imprimindo-diagonal-matriz.c

--- imprimindo-diagonal-matriz.c
+++ imprimindo-diagonal-matriz.c
@@ -2,30 +2,30 @@
 apenas os elementos da diagonal principal.*/
 
 #include <stdio.h>
-
+#define t 3
 
 int main(){
-   int mat[3][3];
+   int mat[t][t];
     printf("\tDigite os valores da matriz:\n");
-    for(int i=0; i<3; i++){
-       for(int j=0; j<3; j++){
+    for(int i=0; i<t; i++){
+       for(int j=0; j<t; j++){
            printf(" Posicao [%d][%d]: ",i, j);
            scanf("%d", &mat[i][j]);
        }
    }
    printf("\n------------------------");
     printf("\nImprimindo a matriz: \n");
-     for(int i=0; i<3; i++){
+     for(int i=0; i<t; i++){
          printf("\t");
-       for(int j=0; j<3; j++){
+       for(int j=0; j<t; j++){
            printf("%d ", mat[i][j]);
        }
        printf("\n");
    }
    printf("------------------------");
    printf("\nDiagonal da Matriz: \n");
-   for(int i=0; i<3; i++){
-       for(int j=0; j<3; j++){
+   for(int i=0; i<t; i++){
+       for(int j=0; j<t; j++){
            if(j==i)
            printf(" %d ", mat[i][j]);
        }
