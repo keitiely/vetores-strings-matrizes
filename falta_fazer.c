b94adf15557bf18b9f9d1d1ee640b06ba0520448
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-10-28 21:19:59 -0300

    Create falta_fazer.c

---
+++ falta_fazer.c
@@ -0,0 +1,7 @@
+//falta a 14, 16, 17 e 18//
+/*14-Codifique um programa para jogar campo minado.
+O programa deve solicitar uma posição ao usuário e mostrar o campo,
+repetidamente, até que a posição 0,0 seja fornecida ou que o usuário jogue numa posição minada.
+representando um campo minado 88. Os limites da matriz devem ser preenchidos com uns e o interior, com 0 ́s
+nas posições livres e 9 ́s nas posições contendo bombas. Utilize um parâmetro adicional na sua função para especificar
+a "facilidade" do campo minado, de tal modo que quanto maior for a facilidade, menor seja número de bombas no campo.*/
